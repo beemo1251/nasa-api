66af4527d924a21c7509708293a2049b72036377
Luis Cordova <luiscordova@ftb.pe>
2024-03-20 16:47:13 -0500

    testing files

--- src/app/services/nasa-api.service.spec.ts
+++ src/app/services/nasa-api.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NasaApiService } from './nasa-api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('NasaApiService', () => {
   let service: NasaApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(NasaApiService);
   });
 
